33dd828e53e64b52f4c576a17b1ee8294ca232b7
Steven Bosnick <sbosnick@sympatico.ca>
2016-11-28 22:27:30 -0500

    Added tests to help diagnose failing gucumber step.
    Added new unit and integration tests to examine possible reasons for a
    failing step in the gucumber acceptance tests.
    
    The new integration tests in build_test.go are a temporary measure and
    have spurious failures if run concurently with the unit test in the
    domain package.

--- apiservice/apiservice_test.go
+++ apiservice/apiservice_test.go
@@ -10,6 +10,7 @@ import (
 
 	"golang.org/x/net/context"
 
+	jsh "github.com/derekdowling/go-json-spec-handler"
 	"github.com/sbosnick1/openacct/domain"
 	"github.com/stretchr/testify/assert"
 )
@@ -48,4 +49,30 @@ func TestNewListsAllFunds(t *testing.T) {
 
 	assert.Equal(http.StatusOK, responsewriter.Code, "Unexpected status code.")
 	assert.True(fakerepository.getAllCalled, "Listing the funds failed to call GetAll()")
+}
+
+func TestListingFundsOnNewApiServiceIncludesAttributes(t *testing.T) {
+	assert := assert.New(t)
+	request, responsewriter := getRequestResponse(t, "/v1/fund")
+	fakerepository := newFakeFundRepository([]fakeFund{{1, domain.CAD, "General"},
+		{2, domain.USD, "Special"}})
+	fakestore := &fakeStore{fakerepository}
+
+	sut := New(fakestore)
+	sut.ServeHTTP(responsewriter, request)
+
+	doc := parseResponseBody(t, responsewriter, jsh.ListMode)
+	assert.True(doc.HasData(), "Returned document unexpected has no data.")
+	for _, obj := range doc.Data {
+		switch obj.ID {
+		case "1":
+			assert.JSONEq(`{"currency" : "CAD", "name" : "General"}`,
+				string(obj.Attributes),
+				"Unexpected attributes on an object in the returned document.")
+		case "2":
+			assert.JSONEq(`{"currency" : "USD", "name" : "Special"}`,
+				string(obj.Attributes),
+				"Unexpected attributes on an object in the returned document.")
+		}
+	}
 }
